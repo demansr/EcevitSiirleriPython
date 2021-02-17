74b47fb0027b37c9f0e8959a3fea8d3544581864
demansr <53873976+demansr@users.noreply.github.com>
2021-02-17 21:52:19 +0300

    Add files via upload

---
+++ poem.py
@@ -0,0 +1,373 @@
+import random
+def get_poem():
+    canakkale = """
+            Çanakkale
+Söyle Arkadaşım' dedi Anadolulu Mehmet
+yanı başındaki Anzak erine
+'nereden kopup gelmişsin,
+neden çökmüş bu mahzunluk üzerine? '
+
+'DÜNYANIN ÖBÜR UCUNDAN' dedi gencecik Anzak
+'Öyle yazmışlar mezar taşıma.
+doğduğum yerler öylesine uzak,
+örtündüğüm topraksa gurbet bana.'
+'Dert edinme arkadaşım' dedi Mehmet
+'değil mi ki bizlerle birleşti kaderin,
+değil mi ki yurdumuzun koynundasın ilelebet,
+sen de artık bizdensin,
+sen de bencileyin bir Mehmet'
+
+Çanakkale'de toprağının
+üstü cennet altı mezar
+kavga bitmiş mezarlarda
+kaynaş olmuş yiten canlar.
+
+'ya sen dedi Mehmet
+oyun çağındaki İngiliz erine,
+'yaşın ne senin kardeş
+böylesine erken buralarda işin ne? '
+'yaşım sonsuza dek on beş'
+dedi ufak tefek İngiliz eri.
+'köyümde askercilik oynar
+coştururdum trampetimle bizimkileri
+derken kendimi cephede buldum
+oyun muydu, gerçek miydi anlamadan,
+bir sahici kurşunla vuruldum.
+Sustu boynumdaki trampet,
+son verildi böylece oyundan bozma işime
+Gelibolu'da bana da bir mezar kazıldı
+mezar taşıma ON BEŞİNDE TRAMPETÇİ' yazıldı.
+Öyküm de künyem de bundan ibaret.'
+
+Yağmur yağıyordu usul usul toprağa
+göz yaşları düşerek üstüne sanki
+damla damla ağlıyordu uzaktan uzağa
+sahibini yitiren bir trampet.
+
+'ya sizler' dedi Mehmet
+dünyanın dört kıtasından
+mezarlar dolusu erlere,
+'hangi rüzgar savurdu sizleri
+bu bilmediğiniz yerlere'
+
+kimi İngilizdi, kimi İskoç
+kimi Fransızdı, kimi Senegalli
+kimi Hintli kimi Nepalli
+kimi Avustralya'dan kimi yeni Zelanda'dan Anzak
+gemiler dolusu asker
+her biri niye geldiğinden habersiz
+Gelibolu'nun oya gibi koylarından sızarak
+tırmanmışlardı dağa bayıra
+siper siper yara gibi yarılan toprak
+mezar olmuştu savaş ardından onlara.
+Kiminin BURADA YATTIĞI SANILIR
+Kiminin ADI BİLİNSE DE MEZARI BİLİNMEZ
+kiminin de mezar taşında
+on altı on yedi on sekiz yaşında
+EBEDİ İSTİRAHATE ÇEKİLDİĞİ yazılı.
+Çanakkale topraklarında,
+her birinin erken biten yaşam öyküsü
+eski yazıtlar gibi taşlara böyle kazılı.
+'Anlamaz mıyım' dedi 'halinizden kardeşler'
+adına yazılı taşı bile olmayan asker
+Anadolulu Mehmet
+'ben de yüz yıllarca yaban ellerde
+neyin uğruna bilmeden can vermişim
+kendi yurdum uğruna can vermenin tadına
+ilk kez Çanakkale'de ermişim.
+Uğrunda can verdikçe vatandı ancak
+ekip biçtiğim padişah mülkü toprak
+değil mi ki sizler alamasanız bile
+bu topraklar almış sizi sizleri basmış bağrına
+sizlere de vatan sayılır artık Çanakkale.
+
+Çanakkale'de toprağının
+üstü cennet altı mezar
+kavga bitmiş mezarlarda
+kaynaş olmuş yiten canlar.
+
+Bir garip savaştı Çanakkale savaşı
+kızıştıkça kızgınlığı dindiren
+ara verildikçe ateşe
+düşmanı kardeşe
+döndüren bir savaştı.
+Kıyasıya bir savaştı
+ama saygı üreten bir savaş
+yaklaştıkça birbirine
+karşılıklı siperler
+gönüller de yakınlaştı
+düştükçe vuruşanlar toprağa
+dostlar gibi kaynaştı.
+
+Savaş bitti.
+Ölenler kaldı sağlar gitti
+köylü köyüne döndü evli evine
+kır çiçekleri geldiler akın akın
+çekilen askerlerin yerine
+yaban gülleri, dağ laleleri, papatyalar,
+kilim kilim yayıldılar toprağa.
+Siper siper
+toprağın savaş yaralarını örttüler
+koyunlar koruganları yuva yaptı kendine
+kuşlar döndü gökyüzüne kurşunların yerine.
+Çiçeğiyle yemişiyle yeşiliyle
+silah yerine saban tutan elleriyle
+geri aldı savaş alanlarını doğa
+can geldi toprağa silindikçe kan izleri.
+Yeryüzünde cennet oldu öylece
+o cehennem savaş yeri
+şimdi Çanakkale Gelibolu
+bahçe bahçe, ülke ülke
+mezar dolu.
+
+Üstü cennet altı mezar
+Çanakkale toprağının
+kavga bitmiş mezarlarda
+kaynaş olmuş yiten canlar.
+
+Huzur içinde uyusun
+vuruştukları toprakta
+kavgadan kinden uzakta
+yan yana dostça yatanlar.
+    """
+    Pulumurun_yassız_kadini ="""
+    Pülümür'un Yaşsız Kadını
+Pülümürün bir dağ köyünde gördüm onu
+yaşını sordum bir giz gibi güldü
+kimi seksen dedi köylülerden kimi yüz
+yüzüne baktım bir giz gibi güldü
+
+bir asa vardı elinde
+bir solmuş kırallığın
+kadifeden harmanisi üzerinde
+bir hititliydi o bir selçukluydu
+bir ermeniydi bir kürttü
+bir türk
+
+yaşını sordum bir giz gibi güldü
+koluma girdi bir soylu kadınca
+tozlu köy yolunda sürüyerek eteğini
+beni tek gözlü sarayına götürdü
+köy yapısı kulübesinin
+
+zamanı onda yitirdim ben
+yitik zamanlara onda eriştim
+en soylu yoksulluğun toprak döşeli sarayında
+bir taç gibi kondu başıma Türkiyeliliğim.
+    """
+    yapadıgımız="""
+        Yapamadığımız
+-Rahşan´a-
+
+akşam kapı eşiğinde bir terli giysi gibi
+soyunmak vardı derdinden evrenin
+bir entari serinliğini giyinmek
+kendi derdini tespih gibi çekmek elinde
+
+yün örmen vardı akşamları koltuğa gömülü
+karşında polisiye roman okumak vardı
+sorgusuz bakışmak yoruldukça gözlerimiz
+sevinçsiz gülmek üzüntüsüz ağlamak
+
+oturmağa konuklar gelmesi bazen
+çevresinde bir masanın kaygısız
+sıcacık konularda bir demli çay gibi
+bilmedik komşularla konuşmak
+
+dünyamızla uyuşmak vardı
+oyunda sonunu görmeden oynamak
+sevinebilmek kazandığına
+yitirdiğine yerinebilmek
+
+düşünmiyebilmek yoruldukça düşünmekten
+kamaştıkça örtebilmek gözlerini
+düşlerde bile ışıktan sakınarak kendini
+uyayabilmek vardı vaktinde rahat
+    """
+    bach_sonatı="""
+    Bach Sonatı
+ne ben sorayım seni
+ne sen beni sor
+soyunmuş seslerimiz tenden
+boşlukta bir aşk örüyor
+
+ses olmuş duygular
+yaklaşır dalga dalga zamansız
+kavuşsa da seslerimiz birbirine
+biz kavuşamayız
+
+ne kollarımız var saracak
+ne öpecek dudaklar
+ne görülecek yüzümüz var
+ne görecek göz
+
+bir aşk örüyoruz boşlukta
+çizgiden soyut
+zerreden öz
+    """
+    cocuk = """
+        Çocuk
+düşünebilseydin eğer doğduğunda
+örtülmeden öğreneceklerinle bildiğin
+konuşabilseydin ağlamanı kesip
+belki de birşeyler öğretebilirdin
+    """
+    yarin="""
+    Yarın
+Bir şeyler olacak yarın
+duruşundan belli
+kırdaki atların
+bulutların koşuşundan belli
+kazışından köstebeklerin toprağı
+
+karıncaların telâşından belli
+bir şeyler olacak yarın
+belki bir tomurcuk
+belki bir ağacın düşen yaprağı
+belki de bir çocuk
+
+pek o kadar göremesek de uzağı
+kuşların uçuşundan belli
+birşeyler olacak yarın
+öbürgünden önemsiz
+yarından önemli
+    """
+    yasa = """
+Elmalarda dis izi
+senindir bu dislem
+yapildi hanene
+gereken islem
+
+melekler de tanik
+suclusun
+isbu yasa hukmunce
+sen bir insanoglusun
+
+insanoglu
+
+MADDE BIR
+dunyaya gelmelidir
+
+MADDE IKI
+sevmeli sevilmeli
+dunyayi cennetin
+kendisi bilmelidir
+
+MADDE UC
+yasama sevgisinin
+kokleri gonlunde
+insanoglu gunun birinde
+olmelidir
+
+donmelidir dudaklarina
+buruk bir elmanin tadi 
+   """
+    aydaki_adam = """
+        Aydaki Adam
+aya giden adamlar
+kovdular aydaki adamı
+aya giden adamların ayak izinden
+aydaki adam gelecek yine bir gün
+inecek yine geceleyin aydan
+aydınlatacak yine uykumu 
+    """
+    insan = """
+        İnsan
+ elbette senden güzel olacaktı
+ çizdiğin resim
+ yaptığın heykel
+ senden büyük olacaktı
+ senden yakışıklı
+
+ elbette senden doğru söyleyecekti
+ yazdığın şiir
+
+ elbette senden çok duyacaktı
+ söylediğin türkü
+
+ sen olduğundan büyüksün
+ sen olduğundan iyisin
+ sen olduğundan güzel 
+    """   
+    sinir="""
+    Sınır
+ dur yolcu bura sınır
+ yabandır yasaktır ötesi
+ çiçeklerden seçemezsin
+ kokuları renkleri bir bir
+
+ kuşdan pasaport sorulmaz
+ gümrüksüz geçer yüküyle karınca
+ dur yolcu bura sınır
+ sen geçemezsin
+
+ dereye bakma durmaz akar
+ öteden de içer ceylan bu suyu
+ dur yolcu bura sınır
+ sen geçemezsin
+
+ dur yolcu bura sınır
+ ne çizili ne yazılı
+ geçemezsin yine de silemezsin
+ içinde kazılı 
+    """
+    ozgurluk = """
+        Özgürlük
+özgürlüğü yitirdik dostlar
+ardından bir çift sözüm var
+
+havaya benzerdi biraz
+varlığı duyulmazdı özgürlüğün
+yokluğu dayanılmaz
+
+“saklamayın” derdi özgürlük “beni kendinize
+esirgemeyin beni ellerden
+esirgendikçe tükenirim çünkü
+paylaşıldıkça çoğalırım ben”
+
+oysa kendimize kalsın diye özgürlük
+ona bahçelerde duvarlar ördük
+uçup gitti kuş misali bahçelerden
+ne eller gördü hayrını ne biz gördük
+
+“yurttaşlar” derdi özgürlük “bu devleti
+sizler yöneteceksiniz el ele
+yaşatabilmek için beni
+yaşayabilmek için benimle”
+
+oysa dünyalarımız öylesine küçüktü
+devlet öylesine büyük
+yönetilmek öylesine rahattı
+yönetmek öylesine yük
+
+bir seyirlik oyun saydık devleti
+bıraktık oyuncuların eline
+düdük çaldı oyun bitti
+“haydi” denildi “herkes evine”
+
+yok artık dostlar
+ağlamanın yararı
+ellerimizle kazıldı
+özgürlüğün mezarı
+
+kendimizi gömdük içine
+    """
+    sevgi = """
+      Sevgi
+ne varsa konuşmaya değen
+birlikte düşündüğümüzden
+birlikte susabilmek
+yağmuru dinlerken
+
+yanımdaki sensin
+yalnızlığım sen
+
+kendimden bile uzakta
+elim elindeyken
+    """
+
+    poems = [canakkale, Pulumurun_yassız_kadini,yapadıgımız,cocuk,bach_sonatı, yarin, insan, aydaki_adam, yasa, sinir, ozgurluk, sevgi]
+   
+    return random.choice(poems)
+
+print(get_poem())
